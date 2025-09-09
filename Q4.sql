020b4bfd9296eff4b271a73778b425137ca8a2ab
RaashidGIT <148008217+RaashidGIT@users.noreply.github.com>
2025-09-09 14:01:00 +0530

    Create Q4.sql

---
+++ Q4.sql
@@ -0,0 +1,6 @@
+-- 1)  Find the sum of salaries of all employees, the maximum salary, the minimum salary, and the average salary. 
+
+select sum(salary),max(salary),min(salary),avg(salary) from employee;
+
+-- 2)  Count the number of projects handled in each department.
+
