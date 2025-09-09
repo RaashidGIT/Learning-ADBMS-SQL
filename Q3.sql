3ad479be4e9c218e3070396550dcbb1340468a57
RaashidGIT <148008217+RaashidGIT@users.noreply.github.com>
2025-09-09 13:43:04 +0530

    Update and rename Q3.txt to Q3.sql

---
+++ Q3.sql
@@ -0,0 +1,13 @@
+-- 1)  List the records in the Employee table order by salary in ascending/descending order. 
+
+SELECT *
+    -> FROM Employee
+    -> ORDER BY Salary ASC;
+
+SELECT *
+    -> FROM Employee
+    -> ORDER BY Salary DESC;
+
+-- 2)  Display only those Employees whose Dnumber is 30. 
+
+SELECT * FROM EMPLOYEE WHERE DNUMBER = 3;
