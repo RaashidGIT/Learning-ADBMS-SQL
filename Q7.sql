517fb601e0f52ebc50888b8a472201b6967df67e
RaashidGIT <148008217+RaashidGIT@users.noreply.github.com>
2025-09-15 09:53:57 +0530

    Update Q7.sql

--- Q7.sql
+++ Q7.sql
@@ -1,1 +1,22 @@
+-- 1) Make list of all project numbers for projects that involve an employee whose name is “Raju” either as a worker or as a manager of the department that controls project.
 
+SELECT PNUMBER
+FROM WORKS_ON
+WHERE EMPLOYEEID IN (
+    SELECT EMPLOYEEID
+    FROM EMPLOYEE
+    WHERE NAME = 'ALICE'
+)
+UNION
+SELECT P.PNUMBER
+FROM PROJECT P
+JOIN DEPARTMENT D ON P.DNUMBER = D.DNUMBER
+JOIN EMPLOYEE E ON D.MANAGERID = E.EMPLOYEEID
+WHERE E.NAME = 'ALICE';
+
+
++---------+
+| PNUMBER |
++---------+
+|       2 |
++---------+
