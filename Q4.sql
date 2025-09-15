d90cd6b2cfa2326aa0d0e6b39ddf831bf02dfa46
RaashidGIT <148008217+RaashidGIT@users.noreply.github.com>
2025-09-15 09:20:42 +0530

    Update Q4.sql

--- Q4.sql
+++ Q4.sql
@@ -4,3 +4,21 @@ select sum(salary),max(salary),min(salary),avg(salary) from employee;
 
 -- 2)  Count the number of projects handled in each department.
 
+select dnumber as departmentnumber,
+    -> count(pnumber) as numberofprojects from project
+    -> group by dnumber;
+
+-- 3)  Count number of employees working in each department. 
+
+select dnumber as departmentnumber ,
+    -> count(employeeid) as numberofemployees
+    -> from employee group by dnumber; 
+
+select d.dname as departmentname ,
+    -> e.dnumber as departmentnumber,
+    -> count(e.employeeid) as numberofemployee
+    -> from employee e join department d on e.dnumber = d.dnumber
+    -> group by e.dnumber,d.dname;
+
+-- 4) Find the department number and maximum salary of those departments where minimum salary is greater than 10000 rupees.
+
