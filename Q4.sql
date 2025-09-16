59e7358d2f3f47fb8accbb4a0b1c128c81a2d3da
RaashidGIT <148008217+RaashidGIT@users.noreply.github.com>
2025-09-16 13:55:41 +0530

    Update Q4.sql

--- Q4.sql
+++ Q4.sql
@@ -1,3 +1,5 @@
+-- Q-4)  AGGREGATE FUNCTIONS
+
 -- 1)  Find the sum of salaries of all employees, the maximum salary, the minimum salary, and the average salary. 
 
 select sum(salary),max(salary),min(salary),avg(salary) from employee;
